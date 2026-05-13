948ebab43abfae2dd7c6ccff107672f8430c24c3
Carlos Rebollón <crebollon8195@github.com>
2026-05-13 15:53:49 -0500

    fix: remove unused isValidPhoneNumber import

--- components/forms/AccessRequestForm.tsx
+++ components/forms/AccessRequestForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { useTranslations } from "next-intl";
-import PhoneInput, { isValidPhoneNumber } from "react-phone-number-input";
+import PhoneInput from "react-phone-number-input";
 import "react-phone-number-input/style.css";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
